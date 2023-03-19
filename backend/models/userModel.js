55e6ab87bdd7c2f4d276a5c2b495d419bac42124
Hanson Tram <htram.dev@gmail.com>
2023-03-18 22:05:48 -0700

    begin implement authentication

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,34 +1,71 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const validator = require('validator')
 
-const Schema = mongoose.Schema;
+const Schema = mongoose.Schema
 
 const userSchema = new Schema({
   email: {
     type: String,
     required: true,
-    unique: true,
+    unique: true
   },
   password: {
     type: String,
-    required: true,
-  },
-});
+    required: true
+  }
+})
 
 // static signup method
-userSchema.statics.signup = async function (email, password) {
-  const exists = await this.findOne({ email });
+userSchema.statics.signup = async function(email, password) {
 
-  if (exists) {
-    throw Error("Email already in use");
+  // validation
+  if (!email || !password) {
+    throw Error('All fields must be filled')
+  }
+  if (!validator.isEmail(email)) {
+    throw Error('Email not valid')
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw Error('Password not strong enough')
   }
 
-  const salt = await bcrypt.genSalt(10);
-  const hash = await bcrypt.hash(password, salt);
+  const exists = await this.findOne({ email })
 
-  const user = await this.create({ email, password: hash });
+  if (exists) {
+    throw Error('Email already in use')
+  }
 
-  return user;
-};
+  const salt = await bcrypt.genSalt(10)
+  const hash = await bcrypt.hash(password, salt)
 
-module.exports = mongoose.model("User", userSchema);
+  const user = await this.create({ email, password: hash })
+
+  return user
+}
+
+
+// static login method
+userSchema.statics.login = async function(email, password) {
+
+  if (!email || !password) {
+    throw Error('All fields must be filled')
+  }
+
+  // find a user with the same email in db
+  const user = await this.findOne({ email })
+  if (!user) {
+    throw Error('Incorrect email')
+  }
+
+  // find users password that matches in db
+  const match = await bcrypt.compare(password, user.password)
+  if (!match) {
+    throw Error('Incorrect password')
+  }
+
+  return user
+}
+
+
+module.exports = mongoose.model('User', userSchema)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,24 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // pages & components
-import Home from './pages/Home';
-import Navbar from './components/Navbar';
+import Home from "./pages/Home";
+import Login from "./pages/Login"
+import SignUp from "./pages/SignUp"
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Navbar />
+        <Navbar />
         <div className="pages">
           <Routes>
-            <Route
-              path="/"
-              element={ <Home />}
-            />
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
           </Routes>
         </div>
       </BrowserRouter>
-      
     </div>
   );
 }
